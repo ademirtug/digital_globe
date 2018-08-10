63373c82d1b89294355eb5bf36612d36038baaa3
ademirtug <ademirtug@hotmail.com>
2018-08-10 16:19:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
-// 3d_engine.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "camera.h"
 
 using namespace std;
 extern engine eng;
+
+class ellipsoid
+{
+public:
+	float num_lat, num_long;
+	glm::vec3 radii;
+
+
+	ellipsoid(double _x, double _y, double _z)
+	{
+		radii.x = _x;
+		radii.y = _y;
+		radii.z = _z;
+		float num_lat = 9 * 3;
+		float num_long = 18 * 3;
+
+
+	}
+	ellipsoid(glm::vec3 xyz) : ellipsoid(xyz.x, xyz.y, xyz.z)
+	{
+	}
+	vector<glm::vec3> generate_vertices()
+	{
+		glm::vec3** globe = new glm::vec3*[num_lat + 1];
+		for (int i = 0; i < num_lat + 1; ++i)
+			globe[i] = new glm::vec3[num_long + 1];
+
+
+	}
+};
 
 
 glm::vec3 calc_normal(glm::vec3 pt1, glm::vec3 pt2, glm::vec3 pt3)
@@ -16,7 +43,7 @@ glm::vec3 calc_normal(glm::vec3 pt1, glm::vec3 pt2, glm::vec3 pt3)
 
 float N(float phi, float a, float b)
 {
-	return (a*a) / sqrt(  (a*a)*cos(phi)*cos(phi)  +  (b*b)*sin(phi)*sin(phi));
+	return (a*a) / sqrt((a*a)*cos(phi)*cos(phi) + (b*b)*sin(phi)*sin(phi));
 }
 
 int main()
@@ -25,16 +52,18 @@ int main()
 
 	eng.cam = cam;
 	eng.init(1024, 768);
-	eng.maxfps =25;
+	eng.maxfps = 25;
 	//eng.sc->enable_dirlight = false;
+	//cam->cdist = 7000000;
 
 
-	float r = 6.371f;
-	float a = 6.3567523f;
-	float b = 6.3781370f;
+
+	float r = 6371000.0f; 
+	float a = 6356752.3f;
+	float b = 6378137.0f;
 	float h = 0.0f;
-	float num_lat = 90;
-	float num_long = 180;
+	float num_lat = 9 * 3;
+	float num_long = 18 * 3;
 
 	glm::vec3** globe = new glm::vec3*[num_lat + 1];
 	for (int i = 0; i < num_lat + 1; ++i)
@@ -53,7 +82,6 @@ int main()
 
 		for (size_t j = 0; j<num_long + 1; j++)
 		{
-
 			float lon = ((2 * glm::pi<float>() / num_long)*j);
 
 			float x = (N(lat, a, b) + h) * cos(lat) * cos(lon);
@@ -62,29 +90,11 @@ int main()
 
 			float z = (((b*b) / (a*a)) * N(lon, a, b) + h) * sin(lat);
 
-
 			glm::vec3 pt = { x, y, z };
 			globe[i][j] = pt;
 			pc.addpoint(pt);
 		}
 	}
-
-	//for (size_t i = 0; i < num_lat+1; i++)
-	//{
-	//	float lon = ((glm::pi<float>() / num_lat)*i) + glm::pi<float>()/2;
-	//	for (size_t j = 0; j<num_long+1; j++)
-	//	{
-	//		
-	//		float lat = ((2 * glm::pi<float>() / num_long)*j);
-	//		float x = (N(lon, a, b) + h) * cos(lon) * cos(lat);
-	//		float y = (N(lon, a, b) + h) * cos(lon) * sin(lat);
-	//		float z = (((b*b) / (a*a)) * N(lat, a, b) + h) * sin(lon);
-	//		glm::vec3 pt = { x, y, z };
-	//		globe[i][j] = pt;
-	//		pc.addpoint(pt);
-	//	}
-	//}
-
 
 
 	for (size_t i = 0; i < num_lat; i++)
@@ -99,11 +109,9 @@ int main()
 			vertices.push_back(pt2);
 			vertices.push_back(pt3);
 
-
 			normals.push_back(calc_normal(pt1, pt2, pt3));
 			normals.push_back(calc_normal(pt2, pt3, pt1));
 			normals.push_back(calc_normal(pt3, pt1, pt2));
-
 
 			glm::vec3 pt4 = globe[i+1][j];
 			glm::vec3 pt5 = globe[i + 1][j + 1];
@@ -119,49 +127,6 @@ int main()
 		}
 	}
 
-
-	//////sphere
-	////for (size_t i = 0; i < lines +1; i++)
-	////{
-	////	float lon = ((2 * glm::pi<float>() / lines)*i); 
-	////	for (size_t j = 0; j<lines +1 ; j++)
-	////	{
-	////		float lat = ((2 * glm::half_pi<float>() / lines)*j);
-	////		float x = r * sin(lat) * cos(lon);
-	////		
-	////		float y = r * sin(lon) * sin(lat);
-	////		float z = r * cos(lat);
-	////		glm::vec3 pt = {x, y, z};
-	////		globe[j][i] = pt;
-	////		pc.addpoint(pt);	
-	////	}
-	////}
-	////for (size_t i = 0; i < lines; i++)
-	////{
-	////	for (size_t j = 0; j< lines;j++)
-	////	{
-	////		glm::vec3 pt1 = globe[i + 1][j];
-	////		glm::vec3 pt2 = globe[i][j+1];
-	////		glm::vec3 pt3 = globe[i][j];
-	////		vertices.push_back(pt1);
-	////		vertices.push_back(pt2);
-	////		vertices.push_back(pt3);
-	////		normals.push_back(calc_normal(pt1, pt2, pt3));
-	////		normals.push_back(calc_normal(pt2, pt3, pt1));
-	////		normals.push_back(calc_normal(pt3, pt1, pt2));
-	////		glm::vec3 pt4 = globe[i][j + 1];
-	////		glm::vec3 pt5 = globe[i+1][j];
-	////		glm::vec3 pt6 = globe[i + 1][j + 1];
-	////		vertices.push_back(pt4);
-	////		vertices.push_back(pt5);
-	////		vertices.push_back(pt6);
-	////		normals.push_back(calc_normal(pt4, pt5, pt6));
-	////		normals.push_back(calc_normal(pt5, pt6, pt4));
-	////		normals.push_back(calc_normal(pt6, pt4, pt5));
-	////	}
-	////}
-
-	
 	colormesh cm(vertices, normals);
 	cm.position = { 0.0, 0.0, 0.0 };
 	eng.sc->meshes.push_back(&cm);
@@ -177,3 +142,42 @@ int main()
 	return 0;
 }
 
+//////sphere
+////for (size_t i = 0; i < lines +1; i++)
+////{
+////	float lon = ((2 * glm::pi<float>() / lines)*i); 
+////	for (size_t j = 0; j<lines +1 ; j++)
+////	{
+////		float lat = ((2 * glm::half_pi<float>() / lines)*j);
+////		float x = r * sin(lat) * cos(lon);
+////		float y = r * sin(lon) * sin(lat);
+////		float z = r * cos(lat);
+////		glm::vec3 pt = {x, y, z};
+////		globe[j][i] = pt;
+////		pc.addpoint(pt);	
+////	}
+////}
+////for (size_t i = 0; i < lines; i++)
+////{
+////	for (size_t j = 0; j< lines;j++)
+////	{
+////		glm::vec3 pt1 = globe[i + 1][j];
+////		glm::vec3 pt2 = globe[i][j+1];
+////		glm::vec3 pt3 = globe[i][j];
+////		vertices.push_back(pt1);
+////		vertices.push_back(pt2);
+////		vertices.push_back(pt3);
+////		normals.push_back(calc_normal(pt1, pt2, pt3));
+////		normals.push_back(calc_normal(pt2, pt3, pt1));
+////		normals.push_back(calc_normal(pt3, pt1, pt2));
+////		glm::vec3 pt4 = globe[i][j + 1];
+////		glm::vec3 pt5 = globe[i+1][j];
+////		glm::vec3 pt6 = globe[i + 1][j + 1];
+////		vertices.push_back(pt4);
+////		vertices.push_back(pt5);
+////		vertices.push_back(pt6);
+////		normals.push_back(calc_normal(pt4, pt5, pt6));
+////		normals.push_back(calc_normal(pt5, pt6, pt4));
+////		normals.push_back(calc_normal(pt6, pt4, pt5));
+////	}
+////}
